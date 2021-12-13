e0467fa8ef7ecf775263dad0c0bb77563374a712
reisemh3 <reisemh3@gmail.com>
2021-12-13 20:35:44 +0100

    Three commit

--- TOH/src/app/app.module.ts
+++ TOH/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
 @NgModule({
   declarations: [
